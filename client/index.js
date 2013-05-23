4f66edb08bdc536ebf6b73ac7afbd0fc0f2133f0
joseph elsherbini <joseph.elsherbini@gmail.com>
2013-05-23 14:53:02 -0400

    first-commit

---
+++ client/index.js
@@ -0,0 +1,13 @@
+  Template.hello.greeting = function () {
+    return "Welcome to datacomb.";
+  };
+
+  Template.hello.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+
+  console.log(dc);
