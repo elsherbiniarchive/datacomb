698bb5b7b49aa63e87b7edac241b0f21af8adc1a
joseph elsherbini <joseph.elsherbini@gmail.com>
2013-05-28 16:11:20 -0400

    fake data generated to test dahsboard updating

--- client/dashboard.js
+++ client/dashboard.js
@@ -1,129 +1,147 @@
-Meteor.subscribe("datastream", function initializeDashboard() {
+Meteor.startup(function() {
 
-	var tempChart = dc.compositeChart("#temp-chart");
-	var weightChart = dc.lineChart("#weight-chart");
-	var volumeChart = dc.barChart("#volume-chart");
+	Session.set("isInitialized", false);
 
-	var data = BEEDATA.find().fetch();
-	var dateFormat = d3.time.format("%Y/%m/%d %H:%M")
-	var parseDate = dateFormat.parse;
+	Meteor.subscribe("datastream", function renderDashboard() {
 
-	data.forEach(function(d){
-		d.date = parseDate(d.date);
+			 	var tempChart = dc.compositeChart("#temp-chart"),
+				weightChart = dc.lineChart("#weight-chart"),
+				volumeChart = dc.barChart("#volume-chart"),
+				data = BEEDATA.find().fetch(),
+				dateFormat = d3.time.format("%Y/%m/%d %H:%M"),
+				parseDate = dateFormat.parse;
+
+				data.forEach(function(d) {
+					d.date = parseDate(d.date);
+				});
+
+				var beeData = crossfilter(data),
+				all = beeData.groupAll(),
+				dataByTimepoint = beeData.dimension(function(d) {
+					return d.date;
+				});
+
+				var timepointGroup = dataByTimepoint.group().reduce(
+					//add
+					function (p,v){
+						p.count += 1;
+						p.weight += +v.weight;
+						p.hiveTemp += +v.hiveTemp;
+						p.ambientTemp += +v.ambientTemp;
+						return p;
+					},
+					//remove
+					function (p,v){
+						p.count -= 1;
+						p.weight -= +v.weight;
+						p.hiveTemp -= +v.hiveTemp;
+						p.ambientTemp -= +v.ambientTemp;
+						return p;
+					},
+					//initial
+					function(){
+						return {count: 0, weight: 0, hiveTemp: 0, ambientTemp: 0};
+					}
+				);
+
+				tempChart.width(445)
+					.height(180)
+					.transitionDuration(1000)
+					.margins({top:10, right:50, bottom: 25, left: 40})
+					.dimension(dataByTimepoint)
+					.group(timepointGroup)
+					.valueAccessor(function (d) {
+						return d.value.hiveTemp;
+					})
+					.x(d3.time.scale().domain(d3.extent(data, function(d) {return d.date;})))
+					.xUnits(d3.time.minutes)
+					.elasticY(true)
+					.renderHorizontalGridLines(true)
+					.brushOn(false)
+					.compose([
+						dc.lineChart(tempChart).group(timepointGroup)
+							.valueAccessor(function (d) {
+								return d.value.hiveTemp;
+							})
+							.title(function (d) {
+									return dateFormat(d.key) + "\n"
+										+ "hive temp: " + d.value.hiveTemp + "\n"
+										+ "ambient temp: " + d.value.ambientTemp;
+							}),
+						dc.lineChart(tempChart).group(timepointGroup)
+							.valueAccessor(function (d) {
+								return d.value.ambientTemp;
+							})
+							.title(function (d) {
+									return dateFormat(d.key) + "\n"
+										+ "hive temp: " + d.value.hiveTemp + " \xB0C\n"
+										+ "ambient temp: " + d.value.ambientTemp + " \xB0C";
+							})
+					])
+					.xAxis();
+
+				weightChart.width(445)
+					.height(180)
+					.transitionDuration(1000)
+					.margins({top:10, right:50, bottom: 25, left: 40})
+					.dimension(dataByTimepoint)
+					.group(timepointGroup)
+					.x(d3.time.scale().domain(d3.extent(data, function(d) {return d.date;})))
+					.xUnits(d3.time.minutes)
+					.elasticY(true)
+					.renderHorizontalGridLines(true)
+					.brushOn(false)
+					.valueAccessor(function (d) {
+							return d.value.weight;
+						})
+					.title(function (d) {
+							return dateFormat(d.key) + "\n"
+								+ "weight: " + d.value.weight +" lbs";
+					})
+					.xAxis();
+
+				volumeChart.width(990)
+					.height(40)
+					.margins({top: 0, right: 50, bottom: 20, left: 40})
+					.dimension(dataByTimepoint)
+					.group(timepointGroup)
+					.centerBar(true)
+					.gap(0)
+					.x(d3.time.scale().domain(d3.extent(data, function(d) {return d.date;})))
+					.round(d3.time.minute.round)
+					.xUnits(d3.time.minutes)
+					.renderlet(function (chart) {
+						chart.select("g.y").style("display", "none");
+						tempChart.filter(chart.filter());
+						weightChart.filter(chart.filter());
+					})
+					.on("filtered", function (chart) {
+						dc.events.trigger(function () {
+							tempChart.focus(chart.filter());
+							weightChart.focus(chart.filter());
+						});
+					});
+
+				dc.renderAll();
+				Session.set("isInitialized", true);
+
+				Deps.autorun( function(){
+						
+						var numberOfRecords = beeData.size(),					 	
+					 	newData = BEEDATA.find({},{sort:{date: 1}, skip: numberOfRecords}).fetch();
+					 	console.log(newData);
+					 	//redraw all the charts, rendering only the delta
+					 	newData.forEach(function(d){
+					 		d.date = parseDate(d.date);
+					 	});
+
+					 	//adds only new records to the crossfilter object
+					 	beeData.add(newData);
+					 	dc.redrawAll();
+				});
 		});
-
-	var beeData = crossfilter(data);
-	var all = beeData.groupAll();
-
-	var dataByTimepoint = beeData.dimension(function(d){
-		return d.date;
-	})
-
-	//BEEDATA fields
-	//"date","weight","hiveTemp","ambientTemp","ambientFahrenheit","windDirection","windSpeed"
-	//"windGustSpeed","dewpointFahrenheit","relativeHumidity","pressure","pressureTrend"
-
-	var timepointGroup = dataByTimepoint.group().reduce(
-		//add
-		function (p,v){
-			p.count += 1;
-			p.weight += +v.weight;
-			p.hiveTemp += +v.hiveTemp;
-			p.ambientTemp += +v.ambientTemp;
-			return p;
-		},
-		//remove
-		function (p,v){
-			p.count -= 1;
-			p.weight -= +v.weight;
-			p.hiveTemp -= +v.hiveTemp;
-			p.ambientTemp -= +v.ambientTemp;
-			return p;
-		},
-		//initial
-		function(){
-			return {count: 0, weight: 0, hiveTemp: 0, ambientTemp: 0};
-		}
-	);
-
-	tempChart.width(445)
-		.height(180)
-		.transitionDuration(1000)
-		.margins({top:10, right:50, bottom: 25, left: 40})
-		.dimension(dataByTimepoint)
-		.group(timepointGroup)
-		.valueAccessor(function (d) {
-			return d.value.hiveTemp;
-		})
-		.x(d3.time.scale().domain(d3.extent(data, function(d) {return d.date;})))
-		.xUnits(d3.time.minutes)
-		.elasticY(true)
-		.renderHorizontalGridLines(true)
-		.brushOn(false)
-		.compose([
-			dc.lineChart(tempChart).group(timepointGroup)
-				.valueAccessor(function (d) {
-					return d.value.hiveTemp;
-				})
-				.title(function (d) {
-						return dateFormat(d.key) + "\n"
-							+ "hive temp: " + d.value.hiveTemp + "\n"
-							+ "ambient temp: " + d.value.ambientTemp;
-				}),
-			dc.lineChart(tempChart).group(timepointGroup)
-				.valueAccessor(function (d) {
-					return d.value.ambientTemp;
-				})
-				.title(function (d) {
-						return dateFormat(d.key) + "\n"
-							+ "hive temp: " + d.value.hiveTemp + " \xB0C\n"
-							+ "ambient temp: " + d.value.ambientTemp + " \xB0C";
-				})
-		])
-		.xAxis();
-
-	weightChart.width(445)
-		.height(180)
-		.transitionDuration(1000)
-		.margins({top:10, right:50, bottom: 25, left: 40})
-		.dimension(dataByTimepoint)
-		.group(timepointGroup)
-		.x(d3.time.scale().domain(d3.extent(data, function(d) {return d.date;})))
-		.xUnits(d3.time.minutes)
-		.elasticY(true)
-		.renderHorizontalGridLines(true)
-		.brushOn(false)
-		.valueAccessor(function (d) {
-				return d.value.weight;
-			})
-		.title(function (d) {
-				return dateFormat(d.key) + "\n"
-					+ "weight: " + d.value.weight +" lbs";
-		})
-		.xAxis();
-
-	volumeChart.width(990)
-		.height(40)
-		.margins({top: 0, right: 50, bottom: 20, left: 40})
-		.dimension(dataByTimepoint)
-		.group(timepointGroup)
-		.centerBar(true)
-		.gap(0)
-		.x(d3.time.scale().domain(d3.extent(data, function(d) {return d.date;})))
-		.round(d3.time.minute.round)
-		.xUnits(d3.time.minutes)
-		.renderlet(function (chart) {
-			chart.select("g.y").style("display", "none");
-			tempChart.filter(chart.filter());
-			weightChart.filter(chart.filter());
-		})
-		.on("filtered", function (chart) {
-			dc.events.trigger(function () {
-				tempChart.focus(chart.filter());
-				weightChart.focus(chart.filter());
-			});
-		});
-
-	dc.renderAll();
 });
+
+ 
+
 
--- server/server.js
+++ server/server.js
@@ -1,4 +1,86 @@
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+Meteor.startup( function () {
 
+	//get the date field of the latest entry in the database
+
+
+	Meteor.setInterval(function(){
+		var nextDate;
+		var modelDoc = BEEDATA.find({},{sort: {date: -1}, skip: 287, limit: 1}).fetch()[0];
+
+		modelDate = {
+			year: +modelDoc.date.slice(0,4),
+			month: +modelDoc.date.slice(5,7),
+			day: +modelDoc.date.slice(8,10),
+			hour: +modelDoc.date.slice(11,13),
+			minute: +modelDoc.date.slice(14,16)
+		};
+
+		nextDate = modelDate;
+
+		if (modelDate.minute === 55){
+			if (modelDate.hour === 23){
+				if ((modelDate.day === 28 && modelDate.month === 2) || (modelDate.day === 30 && (modelDate.month === 4 || modelDate.month === 6 || modelDate.month === 9 || modelDate.month === 11)) || modelDate.day === 31){
+					if(modelDate.month === 12){
+						nextDate.year = 1;
+						nextDate.month = 0;
+						nextDate.day = 1;
+						nextDate.hour = 0;
+						nextDate.minute = 0;
+
+					} else {
+						nextDate.month += 1;
+						nextDate.day = 1;
+						nextDate.hour = 0;
+						nextDate.minute = 0;
+					}
+				} else {
+					nextDate.day += 2;
+					nextDate.hour = 0;
+					nextDate.minute =0;
+				}
+			}else {
+				nextDate.day += 1
+				nextDate.hour += 1
+				nextDate.minute = 0;
+			}
+		}else {
+			nextDate.day += 1
+			nextDate.minute += 5;
+		}
+
+		nextDate = {
+			year: "" + nextDate.year,
+			month: nextDate.month < 10 ? "0" + nextDate.month : "" + nextDate.month,
+			day: nextDate.day < 10 ? "0" + nextDate.day : "" + nextDate.day,
+			hour: nextDate.hour < 10 ? "0" + nextDate.hour : "" + nextDate.hour,
+			minute: nextDate.minute < 10 ? "0" + nextDate.minute : "" + nextDate.minute
+		};
+
+		var nextDateString = "" + nextDate.year + "/" + nextDate.month + "/" + nextDate.day + " " + nextDate.hour + ":" + nextDate.minute;
+
+
+		var doc = {
+			"date": nextDateString,
+			"weight": +modelDoc.weight + 7.5 + Math.random()*.1,
+			"hiveTemp": +modelDoc.hiveTemp + Math.random()*.5,
+			"ambientTemp": +modelDoc.ambientTemp + Math.random()*.5,
+			"windDirection": modelDoc.windDirection,
+			"windSpeed": +modelDoc.windSpeed,
+			"windGustSpeed": +modelDoc.windGustSpeed,
+			"dewpointFahrenheit": +modelDoc.dewpointFahrenheit,
+			"relativeHumidity": +modelDoc.relativeHumidity,
+			"pressure": +modelDoc.pressure,
+			"pressureTrend": modelDoc.pressureTrend
+
+		}
+
+		doc.ambientFahrenheit = doc.ambientTemp *(9/5) + 32
+		
+		BEEDATA.insert(doc)
+
+
+
+	}, 10000);
+
+
+});
